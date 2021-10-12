49a7c41ef9a737f643ce67a554f76e6b5c95c8fb
Kenjart <90866887+Kenjart@users.noreply.github.com>
2021-10-12 13:20:57 -0600

    Add files via upload

---
+++ Cash_System.py
@@ -0,0 +1,53 @@
+import random
+import turtle
+import func_module
+
+
+
+def main():
+    func_module.art_2()
+
+def cost(): #Function for cost
+    x = float(input("Enter it's cost "))
+    return x
+
+allcost = 0
+a = int(input("How many things are you buying? ")) #USED FOR THE LOOP
+discount = random.choice([10, 20, 30])
+print ("Our current discount is", discount, "%")
+b = 0
+
+for reciept in range(a): #For the receipt
+
+    name_of_item = input("What is the item name? ")
+    from_cost = cost() * 1.05
+
+#randomization
+    if discount == 10:
+        discount = 0.10
+        disccost = from_cost * discount
+        print("You saved", disccost, "dollars")
+    elif discount == 20:
+        discount = 0.20
+        disccost = from_cost * discount
+        print("You saved", disccost, "dollars")
+    elif discount == 30:
+        discount = 0.30
+        disccost = from_cost * discount
+        print("You saved", disccost, "dollars")
+
+    totalcost = from_cost - disccost
+    print ("Item", name_of_item, "Cost", totalcost)
+    allcost = totalcost + allcost
+    print("All the items added up cost (ACCOUNTED FOR GST)", allcost, "currently.") #all the cost of the items added up
+    b = b + 1
+    if b == a:
+        input("Thank you for shopping at H! ")
+        main()
+
+
+
+
+
+
+
---
+++ func_module.py
@@ -0,0 +1,349 @@
+def text_1():
+    print ("Basic text from a function")
+    print (5+9219812982091129090)
+
+
+
+def art_1():
+    import turtle
+    import time
+    w1 = turtle.Screen()
+    amongy = turtle.Turtle()
+
+    #move the turtle
+    amongy.forward(100)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(100)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.left(210)
+
+    amongy.forward(1000)
+
+    amongy.left(180)
+
+    amongy.forward(1000)
+
+    amongy.left(180)
+
+    amongy.forward(30)
+
+    amongy.left(30)
+
+    amongy.forward(30)
+
+    amongy.left(30)
+
+    amongy.forward(30)
+
+    amongy.left(30)
+
+    amongy.forward(30)
+
+    amongy.left(30)
+
+    amongy.forward(30)
+
+    amongy.left(30)
+
+    amongy.forward(30)
+
+    amongy.left(210)
+
+    amongy.forward(1000)
+
+    amongy.left(180)
+
+    amongy.forward(1000)
+
+    amongy.left(150)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+
+    amongy.right(30)
+
+    amongy.forward(30)
+    #literally a fingernail
+
+    w1.exitonclick()
+
+
+def art_2():
+    import turtle
+
+    w2 = turtle.Screen()
+    amongy2 = turtle.Turtle()
+
+    #move the turtle
+
+    amongy2.fillcolor('cyan')
+
+    amongy2.begin_fill()
+
+    amongy2.forward(100)
+
+    amongy2.right(30)
+
+    amongy2.forward(30)
+
+    amongy2.right(30)
+
+    amongy2.forward(30)
+
+    amongy2.right(30)
+
+    amongy2.forward(30)
+
+    amongy2.right(30)
+
+    amongy2.forward(30)
+
+    amongy2.right(30)
+
+    amongy2.forward(30)
+
+    amongy2.right(30)
+
+    amongy2.forward(100)
+
+    amongy2.right(30)
+
+    amongy2.forward(30)
+
+    amongy2.right(30)
+
+    amongy2.forward(30)
+
+    amongy2.right(30)
+
+    amongy2.forward(30)
+
+    amongy2.right(30)
+
+    amongy2.forward(30)
+
+    amongy2.right(30)
+
+    amongy2.forward(30)
+
+    amongy2.right(30)
+
+    amongy2.end_fill()
+
+    amongy2.fillcolor('red')
+
+    amongy2.begin_fill()
+
+    amongy2.forward(25)
+
+    amongy2.left(45)
+
+    amongy2.forward(60)
+
+    amongy2.right(45)
+
+    amongy2.forward(95)
+
+    amongy2.right(45)
+
+    amongy2.forward(60)
+
+    amongy2.right(45)
+
+    amongy2.forward(320)
+
+    amongy2.right(180)
+
+    amongy2.forward(120)
+
+    amongy2.right(90)
+
+    amongy2.forward(60)
+
+    amongy2.left(90)
+
+    amongy2.forward(150)
+
+    amongy2.left(90)
+
+    amongy2.forward(60)
+
+    amongy2.left(90)    
+
+    amongy2.forward(270)
+
+    amongy2.right(90)
+
+    amongy2.forward(60)
+
+    amongy2.right(90)
+
+    amongy2.forward(120)
+
+    amongy2.left(90)
+
+    amongy2.forward(60)
+
+    amongy2.left(90)
+
+    amongy2.forward(120)
+
+    amongy2.right(90)
+
+    amongy2.forward(60)
+
+    amongy2.right(90)
+
+    amongy2.forward(208)
+
+    amongy2.right(90)
+
+    amongy2.forward(75)
+
+    amongy2.left(30)
+
+    amongy2.forward(30)
+
+    amongy2.left(30)
+
+    amongy2.forward(30)
+
+    amongy2.left(30)
+
+    amongy2.forward(30)
+
+    amongy2.left(30)
+
+    amongy2.forward(30)
+
+    amongy2.left(30)
+
+    amongy2.forward(30)
+
+    amongy2.left(30)
+
+    amongy2.forward(100)
+
+    amongy2.left(180)
+
+    amongy2.end_fill()
+
+    amongy2.penup()
+
+    amongy2.goto(-250,250)
+
+    amongy2.pendown()
+
+    #START OF H
+    
+    amongy2.fillcolor('black')
+
+    amongy2.forward(60)
+
+    amongy2.right(90)
+
+    amongy2.forward(60)
+
+    amongy2.right(180)
+
+    amongy2.forward(120)
+
+    amongy2.right(180)
+
+    amongy2.forward(60)
+
+    amongy2.right(90)
+
+    amongy2.forward(60)
+
+    amongy2.right(90)
+
+    amongy2.forward(60)
+
+    amongy2.right(180)
+
+    amongy2.forward(120)
+
+    amongy2.right(180)
+
+    amongy2.forward(60)
+
+    amongy2.right(90)
+
+    #speen
+    amongy2.penup()
+
+    amongy2.goto(-150,250)
+
+    a = 1
+    for spin in range(a):
+        amongy2.right(1)
+        b = a + 1
+    for spin2 in range(25000000):
+        amongy2.right(5)
+        b = b + 1
+    w2.exitonclick()
+art_2()
